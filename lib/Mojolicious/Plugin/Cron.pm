58ebf2c043b72747e4b077c7bf7d6f097d766266
dmanto <dmanto@gmail.com>
2018-05-02 22:13:48 +0000

    first commit

---
+++ lib/Mojolicious/Plugin/Cron.pm
@@ -0,0 +1,23 @@
+use strict;
+use warnings;
+package Mojolicious::Plugin::Cron;
+# ABSTRACT: a Cron-like helper for Mojolicious and Mojolicious::Lite projects
+
+=head1 SYNOPSIS
+
+  ...
+
+=method register
+
+This method does something experimental.
+
+=method method_y
+
+This method returns a reason.
+
+=head1 SEE ALSO
+
+=for :list
+* L<Mojolicious>
+* L<Mojolicious::Plugins>
+1;
