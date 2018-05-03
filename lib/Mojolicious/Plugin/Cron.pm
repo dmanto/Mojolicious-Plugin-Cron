6fe354f6cd46919e4700e68b6d1cd8affbc5e838
dmanto <dmanto@gmail.com>
2018-05-03 19:12:59 +0000

    initial

--- lib/Mojolicious/Plugin/Cron.pm
+++ lib/Mojolicious/Plugin/Cron.pm
@@ -8,23 +8,27 @@ use Carp 'croak';
 
 our $VERSION = "0.001";
 
-# ABSTRACT: a Cron-like helper for Mojolicious and Mojolicious::Lite projects
+1;
+
+
+=head1 NAME
+
+Mojolicious::Plugin::Cron - a Cron-like helper for Mojolicious and Mojolicious::Lite projects
 
 =head1 SYNOPSIS
 
-  ...
+  # Mojolicious::Lite
+  plugin Cron;
 
-=method register
+  cron '*/5 9-17 * * *' => sub {
+      my $c = shift;
+      # do someting non-blocking but useful
+  }
 
-This method does something experimental.
-
-=method method_y
-
-This method returns a reason.
+  get '/' => sub {...}
 
 =head1 SEE ALSO
 
 =for :list
 * L<Mojolicious>
 * L<Mojolicious::Plugins>
-1;
