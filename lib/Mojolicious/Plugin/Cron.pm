4702d4b7025a53c7a4b70ba4e7a370d99b15e7b0
dmanto <dmanto@gmail.com>
2018-05-08 15:37:25 -0300

    Initial version

--- lib/Mojolicious/Plugin/Cron.pm
+++ lib/Mojolicious/Plugin/Cron.pm
@@ -9,6 +9,7 @@ use Carp 'croak';
 our $VERSION = "0.001";
 
 1; 
+=encoding utf8
 
 =head1 NAME
 
@@ -26,8 +27,41 @@ Mojolicious::Plugin::Cron - a Cron-like helper for Mojolicious and Mojolicious::
 
   get '/' => sub {...}
 
+=head1 DESCRIPTION
+
+L<Mojolicious::Plugin::Cron> is a L<Mojolicious> plugin that allows to schedule tasks
+ directly from inside a Mojolicious application.
+You should not consider it as a cron replacement, but as a method to make a proof of
+concept of a project. Cron is really battle-tested, so final version should use it.
+
+=head1 HELPERS
+
+L<Mojolicious::Plugin::Cron> implements the following helpers.
+
+=head2 cron
+
+Define cron tasks.
+
+# schedule non-blocking task every 10 minutes
+$app->cron('*/10 * * * *' => sub {...});
+
+=head1 METHODS
+
+=head2 register
+
+    $plugin->register
+
+=head1 AUTHOR
+
+Daniel Mantovani, C<dmanto@cpan.org>
+
+=head1 COPYRIGHT AND LICENCE
+
+Copyright 2018, Daniel Mantovani.
+
+This library is free software; you may redistribute it and/or modify it under
+the terms of the Artistic License version 2.0.
+
 =head1 SEE ALSO
 
-=for :list
-* L<Mojolicious>
-* L<Mojolicious::Plugins>
+L<Mojolicious>, L<Mojolicious::Guides>, L<Mojolicious::Plugins>
