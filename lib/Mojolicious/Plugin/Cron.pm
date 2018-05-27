453f5fc48c36f717a21a62704652b022c02091ee
dmanto <dmanto@gmail.com>
2018-05-27 20:12:37 -0300

    Checking in changes prior to tagging of version 0.017.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index dc4941c..ae977a8 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,6 +1,8 @@
     Revision history for Perl extension Minilla
    
     {{$NEXT}}
    +
    +0.017 2018-05-27T23:12:25Z
         - minimal version on cpanfile requirements
     0.016 2018-05-27T17:09:48Z
         - adds simple example

--- lib/Mojolicious/Plugin/Cron.pm
+++ lib/Mojolicious/Plugin/Cron.pm
@@ -11,7 +11,7 @@ use Algorithm::Cron;
 
 use Carp 'croak';
 
-our $VERSION = "0.016";
+our $VERSION = "0.017";
 use constant CRON_DIR => 'mojo_cron_dir';
 use constant CRON_WINDOW => 20;    # 20 segs window lock semaphore taken
 my $crondir;
